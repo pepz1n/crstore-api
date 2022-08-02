b6bcae7a55256320d7b4f86dca60c28f9fc28cc0
pepz1n <bernardo.zanetti@unochapeco.edu.br>
2022-08-02 11:26:29 -0300

    forget

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,13 @@
 import usersRoute from "./usersRoute";
 import categoryRoute from "./categoryRoute";
 import productRoute from "./productRoute";
+import paymentRoute from "./paymentRoute";
 
 function Routes(app) {
 	usersRoute(app);
 	productRoute(app)
 	categoryRoute(app);
+  paymentRoute(app);
 }
 
 export default Routes;
---
+++ src/routes/paymentRoute.js
@@ -0,0 +1,10 @@
+import controller from '../controllers/paymentController'
+import Authenticate from '../utils/Authenticate'
+
+export default (app) => {
+	app.post('/payment/destroy',controller.delet)
+	app.get('/payment',controller.getAll)
+	app.post('/payment',controller.persist)
+	app.get('/payment/:id', controller.getById)
+	app.post('/payment/:id',controller.persist)
+}
